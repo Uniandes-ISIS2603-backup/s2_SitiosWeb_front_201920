f964a8da89bc24b738ae8b81242e303dc0677b85
nf.abondano <nf.abondano@uniandes.edu.co>
2019-10-29 10:09:07 -0500

    Agregado despliegue de Developer

---
+++ src/app/developer/developer.ts
@@ -0,0 +1,21 @@
+export class Developer {
+  /**
+    * The developer's id
+    */
+    id: number;
+
+    /**
+    * The developer's login
+    */
+    login: string; 
+
+    /**
+    * The developer's email
+    */
+    email: string; 
+
+    /**
+    * The developer's phone
+    */
+    phone: string; 
+}
