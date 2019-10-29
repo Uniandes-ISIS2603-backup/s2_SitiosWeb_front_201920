caaac8f5d8303d08a74faa3c383f399fb002226d
Daniel Galindo Ruiz <d.galindo@uniandes.edu.co>
2019-10-29 10:45:52 -0500

    atributos de projectdetail

--- src/app/project/projectDetail.ts
+++ src/app/project/projectDetail.ts
@@ -1,6 +1,17 @@
-export class ProjectDetail {
-  internalProject: boolean;
-  company: string;
-  id:number;
+import { Project } from "./project";
+import { Iteration } from "../iteration/iteration";
+import { Developer } from "../developer/developer";
+import { InternalSystems } from "../internalsystems/internalsystems"; 
+import { Request } from "../request/request";
+
+export class ProjectDetail extends Project {
+  
+  iterations: Iteration[];
+
+  developers: Developer[];
+
+  internalSystems: InternalSystems[];
+
+  requests: Request[];
 
 }
