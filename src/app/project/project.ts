3eb507061683f423086856931c9fab6d38ce3957
Daniel Galindo Ruiz <d.galindo@uniandes.edu.co>
2019-10-29 10:21:42 -0500

    mas clases

--- src/app/project/project.ts
+++ src/app/project/project.ts
@@ -1,6 +1,17 @@
 export class Project {
+   /**
+    * Says if the Project is internal or not
+    */
   internalProject: boolean;
+
+   /**
+    * The project's company in charge
+    */
   company: string;
+
+   /**
+    * The project's id
+    */
   id:number;
 
 }
