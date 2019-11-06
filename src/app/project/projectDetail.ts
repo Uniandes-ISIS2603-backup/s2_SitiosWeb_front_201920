7a0240c0d5711b148be5f698c94dbd620045a00f
Daniel Galindo Ruiz <d.galindo@uniandes.edu.co>
2019-11-06 18:42:27 -0500

    no se que cambio but gitkraken si

--- src/app/project/projectDetail.ts
+++ src/app/project/projectDetail.ts
@@ -2,6 +2,7 @@ import { Project } from "./project";
 import { Iteration } from "../iteration/iteration";
 import { Developer } from "../developer/developer";
 import {InternalSystems} from '../internalsystems/internalsystems';
+import {Request} from '../request/request';
 export class ProjectDetail extends Project {
   
   /**
