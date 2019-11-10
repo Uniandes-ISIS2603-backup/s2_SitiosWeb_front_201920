35b68bd0c91bf8f53a55b153bacf62bbc989e792
nf.abondano <nf.abondano@uniandes.edu.co>
2019-11-10 16:55:56 -0500

    service

--- src/app/developer/developer.service.ts
+++ src/app/developer/developer.service.ts
@@ -1,32 +1,54 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Developer } from './developer';
-import { DeveloperDetail } from "./developer-detail";
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import {Developer} from './developer';
+import {DeveloperDetail} from './developer-detail';
 
-const API_URL = "../../assets/";
-const developers = 'developers.json';
 
+import {environment} from '../../environments/environment';
+const API_URL = environment.apiURL;
+const developers = '/developers';
+const reviews = '/reviews';
+
+
+/**
+* The service provider for everything related to developers
+*/
 @Injectable()
 export class DeveloperService {
 
-  constructor(private http: HttpClient) { }
+    /**
+    * Constructor of the service
+    * @param http The HttpClient - This is necessary in order to perform requests
+    */
+    constructor(private http: HttpClient) {}
 
-  getDevelopers() : Observable<Developer[]> {
-    return this.http.get<Developer[]>(API_URL + developers);
-  }
+    /**
+    * Returns the Observable object containing the list of developers retrieved from the API
+    * @returns The list of developers in real time
+    */
+    getDevelopers(): Observable<Developer[]> {
+        return this.http.get<Developer[]>(API_URL + developers);
+    }
 
-  getDeveloperDetail(developerId):Observable<DeveloperDetail> {
-    return this.http.get<DeveloperDetail>(API_URL+"developer-"+developerId+".json")
-  }
+    /**
+    * Creates a new developer
+    * @param developer The new developer
+    * @returns The developer with its new id if it was created, false if it wasn't
+    */
+    createDeveloper(developer): Observable<DeveloperDetail> {
+        return this.http.post<DeveloperDetail>(API_URL + developers, developer);
+    }
 
-  createDeveloper(developer): Observable<Developer> {
-    return this.http.post<Developer>(API_URL + developers, developer);
-  }
+    /**
+    * Returns the Observable object with the details of an author retrieved from the API
+    * @returns The author details
+    */
+    getDeveloperDetail(developerId): Observable<DeveloperDetail> {
+        return this.http.get<DeveloperDetail>(API_URL + developers + '/' + developerId);
+    }
 
-  //getProjects() : Observable<Project[]>{
-
-  //}
-  
+    
 }
+
