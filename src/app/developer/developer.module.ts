383c12e5cb20aa7c2cf347dc85618befe59c916b
Daniel Galindo Ruiz <d.galindo@uniandes.edu.co>
2019-11-10 18:57:20 -0500

    Más arreglos

--- src/app/developer/developer.module.ts
+++ src/app/developer/developer.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { DeveloperListComponent } from './developer-list/developer-list.component';
 import { DeveloperService } from './developer.service';
@@ -8,7 +8,7 @@ import { DeveloperDetailComponent } from './developer-detail/developer-detail.co
 import { DeveloperCreateComponent } from './developer-create/developer-create.component';
 
 @NgModule({
-  imports: [ AppRoutingModule, CommonModule, FormsModule],
+  imports: [ AppRoutingModule, CommonModule, ReactiveFormsModule],
   declarations: [ DeveloperListComponent, DeveloperDetailComponent, DeveloperCreateComponent ],
   exports: [ DeveloperListComponent, DeveloperCreateComponent ],
   providers: [ DeveloperService ]
