77217a308a94371a166652c2fc8b98959348734b
Daniel Galindo Ruiz <d.galindo@uniandes.edu.co>
2019-11-10 18:24:20 -0500

    halp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 import {NgxPaginationModule} from 'ngx-pagination';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {ToastrModule} from 'ngx-toastr';
@@ -42,6 +42,7 @@ import { BarraComponent } from './barra/barra.component';
         BrowserAnimationsModule,
         ModalDialogModule.forRoot(),
         AuthModule,
+        ReactiveFormsModule,
         FormsModule,
         HardwareModule,
         ToastrModule.forRoot({
