d59cfa4d4ae35e4cd4f2af4d7ce1a6bcc52627fb
Andres Orozco <af.orozcog@uniandes.edu.co>
2019-11-10 21:05:10 -0500

    se inicia otra vez iteration

--- src/app/iteration/iteration.service.ts
+++ src/app/iteration/iteration.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Iteration} from './iteration';
-import {Observable} from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Iteration } from './iteration';
+import { Observable } from 'rxjs';
+import { catchError, map, tap } from "rxjs/operators";
+import { environment } from '../../environments/environment';
 
-const API_URL = '../../assets';
+const API_URL = environment.apiURL;
 const iterations = '/iterations';
 
 @Injectable()
 export class IterationService {
+  httpOptions = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" })
+  };
 
   constructor(public http: HttpClient) { }
 
@@ -16,19 +21,11 @@ export class IterationService {
   }
 
   getIterationDetail(iterationId): Observable<Iteration>{
-    return this.http.get<Iteration>(API_URL+"iteration-"+iterationId +".json");
+    return this.http.get<Iteration>(API_URL+ iterations + '/' + iterationId);
   }
 
-  createIteration(iteration):Observable<Iteration>{
-    return this.http.post<Iteration>(API_URL + iterations,iteration);
-  }
-
-  updateIteration(iteration):Observable<Iteration>{
-    return this.http.put<Iteration>(API_URL + iterations + '/'+ iteration.id,iteration);
-  }
-
-  deleteIteration(iterationId): Observable<Iteration>{
-    return this.http.delete<Iteration>(API_URL + iterations + '/' + iterationId);
-  }
+  createProvider(iteration: Iteration): Observable<Iteration> {
+    return this.http.post<Iteration>(API_URL, iteration, this.httpOptions).pipe(tap((iteration: Iteration) => console.log(`added iteration w/ ${iteration.objetive} id=${iteration.id}`)));
+}
 
 }
