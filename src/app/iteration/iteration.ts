be8a897dc9636e9afd7db6eabec9db3d438635e2
Andres Orozco <af.orozcog@uniandes.edu.co>
2019-11-10 20:47:56 -0500

    cambios para agregar la asociación con proyecto

--- src/app/iteration/iteration.ts
+++ src/app/iteration/iteration.ts
@@ -1,12 +1,35 @@
+import {Project} from '../project/project';
+
 export class Iteration {
+  
+  /**
+   * objetivo de la iteración
+   */
   objetive: string;
 
+  /**
+   * fecha de validación de la iteración
+   */
   validationDate:Date;
 
+  /**
+   * cambios que se dieron en la iteración
+   */
   changes:string;
 
+  /**
+   * fecha de inicio de la iteración
+   */
   beginDate: Date;
 
+  /**
+   * fecha de finalización de la iteración
+   */
   endDate: Date;
+
+  /**
+   * proyecto al cual pertence la iteración
+   */
+  projecto: Project;
 
 }
