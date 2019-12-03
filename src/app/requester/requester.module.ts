1039478c3aa389f29395547d6130bf4cd96e5fcd
Sebastian Santos Bautista <s.santosb@uniandes.edu.co>
2019-12-03 02:32:40 -0500

    Arreglos

--- src/app/requester/requester.module.ts
+++ src/app/requester/requester.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { RequesterListComponent } from './requester-list/requester-list.component';
+import { RequesterDetailComponent } from './requester-detail/requester-detail.component';
 import { RequesterService } from './requester.service';
 import { RequesterCreateComponent } from './requester-create/requester-create.component';
 
 @NgModule({
   imports: [ AppRoutingModule, CommonModule, ReactiveFormsModule],
-  declarations: [ RequesterListComponent, RequesterCreateComponent ],
+  declarations: [ RequesterListComponent, RequesterCreateComponent, RequesterDetailComponent ],
   exports: [ RequesterListComponent, RequesterCreateComponent ],
   providers: [ RequesterService ]
 })
