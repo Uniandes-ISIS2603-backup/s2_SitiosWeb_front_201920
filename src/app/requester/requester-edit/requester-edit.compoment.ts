af422845c84342cc31c307d87ccc9def7d1198a3
Sebastian Santos Bautista <s.santosb@uniandes.edu.co>
2019-12-05 03:30:56 -0500

    Arreglo

--- src/app/requester/requester-edit/requester-edit.compoment.ts
+++ src/app/requester/requester-edit/requester-edit.compoment.ts
@@ -3,7 +3,7 @@ import {Component, OnInit, Input, OnChanges, Output, EventEmitter} from '@angula
 import {RequesterService} from '../requester.service';
 import {RequesterDetail} from '../requester-detail';
 import {ToastrService} from 'ngx-toastr';
-import { UnitService } from 'src/app/unit/unit.service';
+import { UnitService } from '.../unit/unit.service';
 import { Unit } from 'src/app/unit/unit';
 
 @Component({
