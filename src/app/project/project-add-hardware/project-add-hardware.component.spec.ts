1952cfb94e7b5e6830729941eae7d353d883dcad
Sebastian Santos Bautista <s.santosb@uniandes.edu.co>
2019-12-04 21:29:11 -0500

    Hw

---
+++ src/app/project/project-add-hardware/project-add-hardware.component.spec.ts
@@ -0,0 +1,45 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {APP_BASE_HREF} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+
+import {ActivatedRoute, convertToParamMap} from '@angular/router';
+
+import {AppModule} from '../../app.module';
+import { ProjectAddHardwareComponent } from './project-add-hardware.component';
+import {AppRoutingModule} from '../../app-routing/app-routing.module';
+import {ProjectService} from '../project.service';
+import {Project} from '../project';
+
+describe('BookAddReviewComponent', () => {
+  let component: ProjectAddHardwareComponent;
+    let fixture: ComponentFixture<ProjectAddHardwareComponent>;
+    
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [AppRoutingModule, HttpClientModule, AppModule],
+            declarations: [],
+            providers: [
+                {
+                    provide: APP_BASE_HREF,
+                    useValue: ''
+                }, 
+                ProjectService,
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: {
+                           paramMap: convertToParamMap({id: 100})
+                        }
+                    }
+                }
+            ]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ProjectAddHardwareComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+    
+});
